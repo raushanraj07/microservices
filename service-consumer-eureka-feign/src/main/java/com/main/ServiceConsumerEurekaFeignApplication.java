4f8fdf82d76efc445ea29978178aa1cf386f263e
raushanraj07 <99873893+raushanraj07@users.noreply.github.com>
2022-10-18 11:54:09 +0000

    Add files via upload

---
+++ service-consumer-eureka-feign/src/main/java/com/main/ServiceConsumerEurekaFeignApplication.java
@@ -0,0 +1,15 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class ServiceConsumerEurekaFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConsumerEurekaFeignApplication.class, args);
+	}
+
+}
